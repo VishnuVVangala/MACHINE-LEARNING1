f7018773a97d185124969e510ec5d8d2c7f1d973
VishnuVVangala <112649545+VishnuVVangala@users.noreply.github.com>
2022-09-01 13:27:02 -0500

    Add files via upload

---
+++ 5th Answer.py
@@ -0,0 +1,9 @@
+from math import pi
+radius = 30
+area_of_circle = pi*radius**2
+print(area_of_circle)
+circumference_of_circle = 2*pi*radius
+print(circumference_of_circle)
+r = float(input())
+area_of_circle = pi*r**2
+print(area_of_circle)
---
+++ 7th Answer.py
@@ -0,0 +1,2 @@
+Sequence = "Name\t Age\t Country\t City\t \nAsabneh\t 250\t Finland\t Helsinki";
+print(Sequence)
